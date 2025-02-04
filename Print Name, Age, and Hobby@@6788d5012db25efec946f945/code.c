aa22decee703b3bcd429fdad745bff29bc60abec
nishi_dhiman <nishidhiman2006@gmail.com>
2025-02-04 10:03:03 +0530

    Solution to Print Name, Age, and Hobby

--- Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
+++ Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
@@ -5,7 +5,7 @@ int main() {
     int a;
     char str2{40};
     scanf("%c %d %c" , &str1 , &a , &str2);
-    printf("Name: \n""Age: \n""Hobby: \n");
+    printf("Name: \n","Age: \n","Hobby: \n");
 
     return 0;
 }
